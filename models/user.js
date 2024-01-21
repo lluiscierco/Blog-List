88fcd90da237b05ea088d5fbf8af7a7a0d40fa99
lluis <63962612+lluiscierco@users.noreply.github.com>
2024-01-21 23:09:54 +0000

    4.17

--- models/user.js
+++ models/user.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-  user: [
+const userSchema = new mongoose.Schema({
+  username: String,
+  passwordHash: String,
+  name: String,
+  blogs: [
     {
       type: mongoose.Schema.Types.ObjectId, // id
-      ref: "User", // reference (link) to table Note
+      ref: "Blog", // reference (link) to table Blog
     },
   ],
 });
 
 // Change toJSON method so it doesnt return id and version when fetching from DB
-blogSchema.set("toJSON", {
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString(); //convert to str as is object
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.passwordHash;
   },
 });
 
-module.exports = mongoose.model("Blog", blogSchema);
+module.exports = mongoose.model("User", userSchema);
--- test_backend.js
+++ test_backend.js
@@ -1,30 +1,58 @@
 const axios = require("axios");
 const apiUrl = "http://localhost:3003/api/blogs";
-/*
+const usersUrl = "http://localhost:3003/api/users";
+
+// Blog DB testing
+
 const blog = {
   title: "LOTR",
   author: "Tolkien",
   url: "Test.com",
   likes: 10,
+  user: "65ada01c71f6a9d577490a9e",
 };
-
+/*
 axios
   .post("http://localhost:3003/api/blogs", blog)
   .then((response) => {
     console.log("success!");
   })
   .catch((error) => console.log("error"));
-
+ */
 axios
   .get(apiUrl)
   .then((response) => {
-    console.log("success", response.data);
+    console.log("success", JSON.stringify(response.data, null, 2));
   })
   .catch((error) => console.log("error", error.response.data));
-*/
+/*
 axios
   .delete(`${apiUrl}/65a16e98418af474427f3b76`)
   .then((response) => {
     console.log("success", response.data);
   })
   .catch((error) => console.log("error", error.response.data));
+  */
+
+// Users DB testing
+user = {
+  username: "Yoyi28",
+  password: "secret.19",
+  name: "George",
+  blogs: ["65a16e98418af474427f3b76"],
+};
+/*
+axios
+  .post(usersUrl, user)
+  .then((response) => {
+    console.log("success!");
+  })
+  .catch((error) => console.log("error"));
+
+axios
+  .get(usersUrl)
+  .then((response) => {
+    console.log("success!", JSON.stringify(response.data, null, 2));
+  })
+  .catch((error) => console.log("error"));
+*/
